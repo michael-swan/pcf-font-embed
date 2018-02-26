f8eb2d1669840409e699c46ee5e5fb9f851b03cb
Michael Swan <mswan@fastmail.com>
2018-02-26 17:48:43 -0600

    Fix unsafePerformIO evaluation order

--- src/Graphics/Text/PCF/Embed.hs
+++ src/Graphics/Text/PCF/Embed.hs
@@ -63,30 +63,31 @@ embedPCFTextColor file opaque blank str = genPCFTextCode =<< (runIO $ do
 genPCFTextCode :: PCFText -> Q Exp
 genPCFTextCode (PCFText gs w h img) = do
     fp <- newName "fp"
-    return $ foldl' AppE (ConE 'PCFText) $
-                  [ ListE $ map (\PCFGlyph{..} ->
-                        foldl' AppE (ConE 'PCFGlyph) [ foldl' AppE (ConE 'Metrics) [ LitE $ IntegerL $ fromIntegral $ metrics_left_sided_bearings glyph_metrics
-                                                                                   , LitE $ IntegerL $ fromIntegral $ metrics_right_sided_bearings glyph_metrics
-                                                                                   , LitE $ IntegerL $ fromIntegral $ metrics_character_width glyph_metrics
-                                                                                   , LitE $ IntegerL $ fromIntegral $ metrics_character_ascent glyph_metrics
-                                                                                   , LitE $ IntegerL $ fromIntegral $ metrics_character_descent glyph_metrics
-                                                                                   , LitE $ IntegerL $ fromIntegral $ metrics_character_attributes glyph_metrics ]
-                                                     , LitE $ CharL glyph_char
-                                                     , LitE $ IntegerL $ fromIntegral glyph_width
-                                                     , LitE $ IntegerL $ fromIntegral glyph_height
-                                                     , LitE $ IntegerL $ fromIntegral glyph_pitch
-                                                     , VarE 'fromStrict
-                                                           `AppE` (VarE 'unsafePerformIO
-                                                                 `AppE` (VarE 'unsafePackAddressLen
-                                                                       `AppE` LitE (IntegerL $ fromIntegral $ B.length glyph_bitmap)
-                                                                             `AppE` LitE (StringPrimL $ B.unpack glyph_bitmap)))]) gs
-                  , LitE $ IntegerL $ fromIntegral w
-                  , LitE $ IntegerL $ fromIntegral h
-                  , VarE 'unsafePerformIO
-                      `AppE` (VarE 'fmap
-                                  `AppE` LamE [VarP fp]
-                                            (VarE 'VS.unsafeFromForeignPtr
-                                               `AppE` VarE fp
-                                               `AppE` LitE (IntegerL 0)
-                                               `AppE` LitE (IntegerL $ fromIntegral $ VS.length img))
-                                  `AppE` (VarE 'newForeignPtr_ `AppE` (ConE 'Ptr `AppE` LitE (StringPrimL $ VS.toList img))))]
+    img_name <- newName "img"
+    return $ VarE 'unsafePerformIO
+               `AppE` DoE [ BindS (VarP img_name) (VarE 'fmap
+                                                    `AppE` LamE [VarP fp]
+                                                              (VarE 'VS.unsafeFromForeignPtr0
+                                                                 `AppE` VarE fp
+                                                                 `AppE` LitE (IntegerL $ fromIntegral $ VS.length img))
+                                                    `AppE` (VarE 'newForeignPtr_ `AppE` (ConE 'Ptr `AppE` LitE (StringPrimL $ VS.toList img))))
+                          , NoBindS $ AppE (VarE 'return) $ foldl' AppE (ConE 'PCFText) $
+                                                                [ ListE $ map (\PCFGlyph{..} ->
+                                                                      foldl' AppE (ConE 'PCFGlyph) [ foldl' AppE (ConE 'Metrics) [ LitE $ IntegerL $ fromIntegral $ metrics_left_sided_bearings glyph_metrics
+                                                                                                                                 , LitE $ IntegerL $ fromIntegral $ metrics_right_sided_bearings glyph_metrics
+                                                                                                                                 , LitE $ IntegerL $ fromIntegral $ metrics_character_width glyph_metrics
+                                                                                                                                 , LitE $ IntegerL $ fromIntegral $ metrics_character_ascent glyph_metrics
+                                                                                                                                 , LitE $ IntegerL $ fromIntegral $ metrics_character_descent glyph_metrics
+                                                                                                                                 , LitE $ IntegerL $ fromIntegral $ metrics_character_attributes glyph_metrics ]
+                                                                                                   , LitE $ CharL glyph_char
+                                                                                                   , LitE $ IntegerL $ fromIntegral glyph_width
+                                                                                                   , LitE $ IntegerL $ fromIntegral glyph_height
+                                                                                                   , LitE $ IntegerL $ fromIntegral glyph_pitch
+                                                                                                   , VarE 'fromStrict
+                                                                                                         `AppE` (VarE 'unsafePerformIO
+                                                                                                               `AppE` (VarE 'unsafePackAddressLen
+                                                                                                                     `AppE` LitE (IntegerL $ fromIntegral $ B.length glyph_bitmap)
+                                                                                                                           `AppE` LitE (StringPrimL $ B.unpack glyph_bitmap)))]) gs
+                                                                , LitE $ IntegerL $ fromIntegral w
+                                                                , LitE $ IntegerL $ fromIntegral h
+                                                                , VarE img_name ]]
